eba7557bd2f2f750b5df526fe510abb974d266cc
Mayara Marques <mayaramarquescosta@outlook.com>
2023-02-16 09:13:01 -0300

    test: Adicionando testes mara Motorcycle

--- tests/integration/motorcycleMock.ts
+++ tests/integration/motorcycleMock.ts
@@ -1,5 +1,7 @@
+const model = 'Honda Cb 600f Hornet';
+
 export const motorcycleMock = {
-  model: 'Honda Cb 600f Hornet',
+  model,
   year: 2005,
   color: 'Yellow',
   status: true,
@@ -10,7 +12,7 @@ export const motorcycleMock = {
 
 export const responseMock = {
   id: '63ece7e1caa1c5f8574769c4',
-  model: 'Honda Cb 600f Hornet',
+  model,
   year: 2005,
   color: 'Yellow',
   status: true,
@@ -18,3 +20,26 @@ export const responseMock = {
   category: 'Street',
   engineCapacity: 600,
 };
+
+export const motorcyclesMockList = [
+  {
+    id: '634852326b35b59438fbea2f',
+    model,
+    year: 2005,
+    color: 'Yellow',
+    status: true,
+    buyValue: 30.000,
+    category: 'Street',
+    engineCapacity: 600,
+  },
+  {
+    id: '634852326b35b59438fbea31',
+    model: 'Honda Cbr 1000rr',
+    year: 2011,
+    color: 'Orange',
+    status: true,
+    buyValue: 59.900,
+    category: 'Street',
+    engineCapacity: 1000,
+  },
+];
--- tests/unit/Services/motorcycleService.test.ts
+++ tests/unit/Services/motorcycleService.test.ts
@@ -1,10 +1,12 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
 import { Model } from 'mongoose';
-import { motorcycleMock, responseMock } from '../../integration/motorcycleMock';
+import { motorcycleMock, responseMock,
+  motorcyclesMockList } from '../../integration/motorcycleMock';
 import MotorcycleService from '../../../src/Services/motorcycleService';
 
 describe('Testa a camada service na rota motorcycles', function () {
+  afterEach(sinon.restore);
   it('Verifica se é possível cadastrar uma nova moto na rota motorcycles', async function () {
     sinon.stub(Model, 'create').resolves(responseMock);
 
@@ -12,5 +14,26 @@ describe('Testa a camada service na rota motorcycles', function () {
     const input = await service.create(motorcycleMock);
 
     expect(input).to.be.deep.equal(responseMock);
+  });
+  it('Verifica se retorna todas as motos', async function () {
+    sinon.stub(Model, 'find').resolves(motorcyclesMockList);
+    const service = new MotorcycleService();
+    const response = await service.findAll();
+    expect(response).to.be.deep.equal(motorcyclesMockList);
+  });
+  it('Verifica se retorna uma moto pelo id', async function () {
+    sinon.stub(Model, 'findOne').resolves(responseMock);
+    const service = new MotorcycleService();
+    const response = await service.findById('63ece7e1caa1c5f8574769c4');
+    expect(response).to.be.deep.equal(responseMock);
+  });
+  it('Verifica se ao colocar id errado retorna o erro', async function () {
+    sinon.stub(Model, 'findOne').resolves(null);
+    const service = new MotorcycleService();
+    try {
+      await service.findById('fdjhfj');
+    } catch (error) {
+      expect((error as Error).message).to.be.deep.equal('Motorcycle not found');
+    }
   });
 });
